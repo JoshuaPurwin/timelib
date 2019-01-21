a749bdf0d6b6cecbb2ec4a1ccbef65d7f68d2c11
teilnehmer <yoshiasdf@hotmail.de>
2019-01-21 10:24:08 +0100

    Programm funktioniert

--- main.c
+++ main.c
@@ -13,10 +13,14 @@
  **/
 int day_of_the_year(int day, int month, int year)
 {
-    int amount = 0;
-    for(int i = 0; i > month;i++)
+    if(exists_date(day, month, year) == 0)
     {
-        amount = amount + get_days_for_month(i+1);
+        return -1;
+    }
+    int amount = 0;
+    for(int i = 0; i < month-1;i++)
+    {
+        amount += get_days_for_month(i+1, year);
     }
     amount = amount + day;
 
@@ -75,12 +79,12 @@ int get_days_for_month(int month, int year)
     if (isLeapyear(year)== 0)
     {
         int monthlen[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-        return monthlen[month+1];
+        return monthlen[month-1];
     }
     else if(isLeapyear(year) == 1)
     {
         int monthlen[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
-        return monthlen[month+1];
+        return monthlen[month-1];
     }
 
     else
@@ -119,6 +123,6 @@ int exists_date(int day,int month, int year)
 
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2400));
     return 0;
 }
